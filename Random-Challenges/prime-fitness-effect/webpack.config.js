7283f14fb4d3f3617da57a2af2df320225707e21
Safa Elmali <tsafaelmali@gmail.com>
2020-11-27 12:37:39 +0300

    feat: webpack added

---
+++ Random-Challenges/prime-fitness-effect/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: { index: path.resolve(__dirname, "src", "index.js") },
+    output: {
+        path: path.resolve(__dirname, "build")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jpg|webp)$/,
+                loader: 'url-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html")
+        })
+    ]
+};
