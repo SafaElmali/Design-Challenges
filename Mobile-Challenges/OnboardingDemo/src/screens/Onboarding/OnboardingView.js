150ad0656b07d19732c6bca17b47f75620b4aa44
Safa Elmali <tsafaelmali@gmail.com>
2021-01-09 02:24:35 +0300

    refactor: refactor styles

--- Mobile-Challenges/OnboardingDemo/src/screens/Onboarding/OnboardingView.js
+++ Mobile-Challenges/OnboardingDemo/src/screens/Onboarding/OnboardingView.js
@@ -19,11 +19,11 @@ const OnboardingView = ({ to, onboardImage, text1, text2, goNextOnboardView, vie
             </View>
             {viewPagerRef ?
                 <View style={{ flex: 0.3, flexDirection: 'row', justifyContent: 'space-around' }}>
-                    <TouchableOpacity onPress={to}>
+                    <TouchableOpacity onPress={to} style={{ height: 30 }}>
                         <Text style={{ fontSize: 16, color: '#7D8496' }}>Skip</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={() => viewPagerRef.current.setPage(page)}>
-                        <View style={{ width: 100, height: 150 }}>
+                    <TouchableOpacity style={{height:50}} onPress={() => viewPagerRef.current.setPage(page)}>
+                        <View style={{ width: 100 }}>
                             <View
                                 style={{
                                     width: 50,
@@ -31,10 +31,8 @@ const OnboardingView = ({ to, onboardImage, text1, text2, goNextOnboardView, vie
                                     justifyContent: 'center',
                                     alignItems: 'center',
                                     alignSelf: 'flex-end',
-                                    marginRight: 20,
                                     backgroundColor: "#FF9963",
                                     borderBottomRightRadius: 20,
-                                    zIndex: 1
                                 }}>
                                 <RightArrow />
                             </View>
@@ -43,7 +41,7 @@ const OnboardingView = ({ to, onboardImage, text1, text2, goNextOnboardView, vie
                                 opacity: 0.1,
                                 position: 'absolute',
                                 top: 10,
-                                right: 10,
+                                right: -10,
                                 zIndex: -1,
                                 width: 50,
                                 height: 50,
@@ -52,36 +50,33 @@ const OnboardingView = ({ to, onboardImage, text1, text2, goNextOnboardView, vie
                         </View>
                     </TouchableOpacity>
                 </View> : (
-                    <View style={{ flex: 0.3, alignItems: 'center', justifyContent: 'center' }}>
+                    <View style={{ flex: 0.3, alignItems: 'center' }}>
                         <TouchableOpacity onPress={to}>
-                            <View style={{ height: 150 }}>
-                                <View
-                                    style={{
-                                        width: 167,
-                                        height: 60,
-                                        flexDirection: 'row',
-                                        justifyContent: 'center',
-                                        alignItems: 'center',
-                                        backgroundColor: "#FF9963",
-                                        borderBottomRightRadius: 20,
-                                        zIndex: 1,
-
-                                    }}>
-                                    <Text style={{ color: '#fff', fontSize: 16, fontWeight: 'bold', marginHorizontal: 15 }}>Get Started</Text>
-                                    <RightArrow />
-                                </View>
-                                <View style={{
-                                    backgroundColor: "#FF9963",
-                                    opacity: 0.1,
-                                    position: 'absolute',
-                                    top: 15,
-                                    left: 15,
-                                    zIndex: -1,
+                            <View
+                                style={{
                                     width: 167,
                                     height: 60,
-                                    borderBottomRightRadius: 20
-                                }} />
+                                    flexDirection: 'row',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    backgroundColor: "#FF9963",
+                                    borderBottomRightRadius: 20,
+
+                                }}>
+                                <Text style={{ color: '#fff', fontSize: 16, fontWeight: 'bold', marginHorizontal: 15 }}>Get Started</Text>
+                                <RightArrow />
                             </View>
+                            <View style={{
+                                backgroundColor: "#FF9963",
+                                opacity: 0.1,
+                                position: 'absolute',
+                                top: 15,
+                                left: 15,
+                                zIndex: -1,
+                                width: 167,
+                                height: 60,
+                                borderBottomRightRadius: 20
+                            }} />
                         </TouchableOpacity>
                     </View>
                 )
